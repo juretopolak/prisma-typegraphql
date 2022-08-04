68588ed42f60e68696ae44cbe2a8938ce18efb61
Jure Topolak <jure.topolak@gmail.com>
2022-08-04 15:35:21 +0200

    Add authChecker - stucked! :)

--- index.ts
+++ index.ts
@@ -1,20 +1,28 @@
 import 'reflect-metadata';
+import express from "express";
+import { ApolloServer, gql } from "apollo-server-express";
 import { buildSchema } from 'type-graphql';
-import { ApolloServer } from 'apollo-server';
 import path from 'path';
 import { PrismaClient } from '@prisma/client';
-
+import { expressjwt as jwt } from "express-jwt";
 import { resolvers } from './prisma/generated/type-graphql';
+import { authChecker } from "./auth/auth-checker"
+import { Context } from './auth/context.interface'
 
-interface Context {
-  prisma: PrismaClient;
-}
+// interface Context {
+//   prisma: PrismaClient;
+//   req: any
+// }
 
 async function main() {
+
+  const app = express();
+
   const schema = await buildSchema({
     resolvers,
     emitSchemaFile: path.resolve(__dirname, './generated-schema.graphql'),
     validate: false,
+    authChecker,
   });
 
   const prisma = new PrismaClient();
@@ -22,10 +30,38 @@ async function main() {
 
   const server = new ApolloServer({
     schema,
-    context: (): Context => ({ prisma }),
+    context: () => {
+      const ctx: Context = {
+        // create mocked user in context
+        // in real app you would be mapping user from `req.user` or sth
+        user: {
+          id: 1,
+          name: "Sample user",
+          roles: ["REGULAR"],
+        },
+      };
+      return ctx;
+    }
+    //context: (): Context => ({ req, prisma }) => {},
   });
-  const { port } = await server.listen(4000);
-  console.log(`GraphQL is listening on ${port}!`);
+
+  const urlPath = "/graphql";
+
+  app.use(
+    urlPath,
+    jwt({
+      secret: "TypeGraphQL",
+      credentialsRequired: false,
+      algorithms: ["HS256"],
+    }),
+  );
+
+  server.applyMiddleware({ app, path: urlPath});
+
+  app.listen({ port: 4000 }, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`),
+  );
+
 }
 
 main().catch(console.error);
